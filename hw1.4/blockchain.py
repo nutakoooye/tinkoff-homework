35f4100975518232da2273f9e8c853cf48ba6a96
nutakoooye <andrkool151@gmail.com>
2022-10-03 09:42:20 +0300

    [hw-1-4] bad variant multiprocessing

--- hw1.4/blockchain.py
+++ hw1.4/blockchain.py
@@ -1,7 +1,7 @@
 import datetime
 import hashlib
-import json
 from flask import Flask, jsonify
+import multiprocessing
 
 
 def math_func(proof: int, previous_proof: int) -> int:
@@ -51,19 +51,21 @@ class Blockchain:
     def get_previous_block(self) -> Block:
         return self.chain[-1]
 
-    def proof_of_work(self, previous_proof):
-        new_proof = 1
+    def proof_of_work(self, previous_proof, start_proof, stop_proof):
+        new_proof = start_proof
         check_proof = False
 
-        while check_proof is False:
+        while check_proof is False and new_proof <= stop_proof:
             hash_operation = get_sha256(new_proof, previous_proof)
 
             if self.is_hash_complex_valid(hash_operation):
                 check_proof = True
             else:
                 new_proof += 1
-
-        return new_proof
+        if check_proof:
+            print("------------------", new_proof)
+            return new_proof
+        return None
 
     def is_hash_complex_valid(self, hash_operation):
         return hash_operation[:len(self.complex)] == self.complex
@@ -90,12 +92,85 @@ class Blockchain:
         return True
 
 
+class CoreMiner(multiprocessing.Process):
+    def __init__(self, task_queue, result_queue):
+        multiprocessing.Process.__init__(self)
+        self.task_queue = task_queue
+        self.result_queue = result_queue
+
+    def run(self):
+        while True:
+            temp_task = self.task_queue.get()
+
+            if temp_task is None:
+                self.task_queue.task_done()
+                break
+
+            answer = temp_task.process()
+            print(answer)
+            self.task_queue.task_done()
+            if answer:
+                print(answer)
+                self.result_queue.put(answer)
+
+
+class Task():
+    def __init__(self,
+                 blockchain: Blockchain,
+                 previous_proof: int,
+                 start_proof: int,
+                 stop_proof: int):
+        self.blockchain = blockchain
+        self.previous_proof = previous_proof
+        self.start_proof = start_proof
+        self.stop_proof = stop_proof
+
+    def process(self):
+        return blockchain.proof_of_work(self.previous_proof,
+                                        self.start_proof,
+                                        self.stop_proof)
+
+
 # user -> www.vk.ru -> login(eyes) - front -> POST username, password ==> backend - АПИ
 
 app = Flask(__name__)
 blockchain = Blockchain(calc_complex="000000")
 
 
+def mining_proof(blockchain:blockchain)->int:
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block.proof
+
+    tasks = multiprocessing.JoinableQueue()
+    results = multiprocessing.Queue()
+
+    n_cores = multiprocessing.cpu_count()
+    miners = [CoreMiner(tasks, results) for i in range(n_cores)]
+
+    start_proof = 1
+    stop_proof = 10 ** (len(blockchain.complex)+1)
+    step = (stop_proof - start_proof) // 20
+    for miner_core in miners:
+        miner_core.start()
+
+    while results.empty():
+        print('new section')
+
+        for proof_section in range(start_proof, stop_proof, step):
+            task = Task(blockchain,
+                        previous_proof,
+                        proof_section,
+                        proof_section+step)
+            tasks.put(task)
+            print(proof_section,proof_section+step)
+        start_proof = stop_proof
+        stop_proof *= 2
+
+        for i in range(n_cores):
+            tasks.put(None)
+        tasks.join()
+
+    return results.get()
 # Graphql, GRPC
 
 # Shop - product API - REST
@@ -104,12 +179,32 @@ blockchain = Blockchain(calc_complex="000000")
 # PATCH - change small product
 # GET - get list product
 
+@app.route("/multiprocessing", methods=["GET"])
+def mp_mine_block():
+    proof = mining_proof(blockchain)
+    previous_block = blockchain.get_previous_block()
+    previous_hash = previous_block.get_hash()
+    block = blockchain.create_block(proof, previous_hash)
+
+    response = {
+        "message": "Block created",
+        "index": block.index,
+        "timestamp": block.timestamp,
+        "proof": block.proof,
+        "previous_hash": block.previous_hash,
+        # "executed time in seconds": executed_time.total_seconds()
+    }
+    return jsonify(response), 200
+
+
 @app.route("/mine_block", methods=["GET"])
 def mine_block():
     previous_block = blockchain.get_previous_block()
     previous_proof = previous_block.proof
-
-    proof = blockchain.proof_of_work(previous_proof)
+    # t_begin = datetime.datetime.now()
+    proof = blockchain.proof_of_work(previous_proof, 1, 10000000000)
+    t_end = datetime.datetime.now()
+    # executed_time = t_end - t_begin
     previous_hash = previous_block.get_hash()
 
     block = blockchain.create_block(proof, previous_hash)
@@ -119,7 +214,8 @@ def mine_block():
         "index": block.index,
         "timestamp": block.timestamp,
         "proof": block.proof,
-        "previous_hash": block.previous_hash
+        "previous_hash": block.previous_hash,
+        # "executed time in seconds": executed_time.total_seconds()
     }
 
     return jsonify(response), 200
